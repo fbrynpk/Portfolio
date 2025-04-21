fe868f2baadbd04898d10c72620394653d20d1b3
fbrynpk <febryanputra12@gmail.com>
2025-04-21 15:12:48 +0800

    Portfolio Name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
+import Head from 'next/head';
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Ryan's Portfolio</title>
+        <meta name="description" content="Clinical Data Scientist & AI Researcher" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
